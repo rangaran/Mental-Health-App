cb9ab0459b1d3e8fb75ada7add3436187c239b04
Niyatha Rangarajan <rangaran@mcmaster.ca>
2022-08-12 14:01:12 -0400

    Landing page changes

--- chatapp/src/components/Signin.js
+++ chatapp/src/components/Signin.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef }  from 'react'
 import { useState } from 'react';
 import Box from '@material-ui/core/Box';
 import firebase from 'firebase/compat/app';
@@ -7,8 +7,15 @@ import { View, StyleSheet, Text } from 'react-native';
 import 'firebase/compat/firestore';
 import {Button} from '@material-ui/core'
 import { auth } from '../firebase.js'
-
+import ReactWordcloud from "react-wordcloud";
 import ReactiveButton from 'reactive-button';
+import words from "./words";
+import { Resizable } from "re-resizable";
+import "d3-transition";
+import { select } from "d3-selection";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
+
 function Signin() {
 
     // const particlesInit = async (main) => {
@@ -20,6 +27,39 @@ function Signin() {
     //     await loadFull(main);
     //   };
     
+const resizeStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  
+  
+};
+function getCallback(callback) {
+  return function (word, event) {
+    const isActive = callback !== "onWordMouseOut";
+    const element = event.target;
+    const text = select(element);
+    text
+      .on("click", () => {
+        if (isActive) {
+          window.open(`https://duckduckgo.com/?q=${word.text}`, "_blank");
+        }
+      })
+      .transition()
+      .attr("background", "white")
+      .attr("font-size", isActive ? "300%" : "100%")
+      .attr("text-decoration", isActive ? "underline" : "none");
+  };
+}
+const reference = useRef();
+const callbacks = {
+  getWordColor: (word) => (word.value > 50 ? "orange" : "lightblue"),
+  getWordTooltip: (word) =>
+    `The word "${word.text}" appears ${word.value} times.`,
+  onWordClick: getCallback("onWordClick"),
+  onWordMouseOut: getCallback("onWordMouseOut"),
+  onWordMouseOver: getCallback("onWordMouseOver")
+};
       const particlesLoaded = (container) => {
         console.log(container);
       };
@@ -33,14 +73,13 @@ function Signin() {
     
         <div style={{ 
       backgroundImage: "url(" + "https://raw.githubusercontent.com/rangaran/rangaran.github.io/main/images/ammu.png" + ")"
-      ,
+      ,backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      minHeight:'120vh',
+      minWidth:'100vh',
+        overflow:'hidden'
   
-  minWidth:'100%',
-  minHeight:'100vh',
-  
-  backgroundSize: 'contain',
-  backgroundRepeat: 'repeat'
-
     }}> 
         
         <View >
@@ -76,8 +115,23 @@ function Signin() {
         <h1>
           
         </h1>
+        <center>
+        
+        <div style={{display: 'flex'}}>
         
         
+        <ReactWordcloud  callbacks={callbacks}
+          words={words}
+          options={{
+            tooltipOptions: {
+              trigger: "click",
+              triggerTarget: reference.current
+            }
+          }}/> 
+          
+      </div>
+
+      </center>
         {/* </div> */}
     </div>
   )
---
+++ chatapp/src/components/words.js
@@ -0,0 +1,378 @@
+export default [
+    {
+   text: 'Explore',      value:64,
+    },
+    {
+   text: 'Firebase',      value:11,
+    },
+    {
+   text: 'Anonymous Texting',      value:16,
+    },
+    {
+   text: 'Google API',      value:17,
+    },
+    {
+   text: 'Anonymous Texting',      value:10,
+    },
+    {
+   text: 'Counselling',      value:54,
+    },
+    {
+   text: 'Anonymous Texting',      value:12,
+    },
+    {
+   text: 'Google Maps',      value:77,
+    },
+    {
+   text: 'Anonymous Texting',      value:45,
+    },
+    {
+   text: 'Shows nearby services on the Map',      value:19,
+    },
+    {
+   text: 'Geolocation',      value:13,
+    },
+    {
+   text: 'React',      value:32,
+    },
+    {
+   text: 'Anonymous Texting',      value:22,
+    },
+    {
+   text: 'Anonymous Texting',      value:35,
+    },
+    {
+   text: 'Firebase',      value:24,
+    },
+    {
+   text: 'Anonymous Texting',      value:38,
+    },
+    {
+   text: 'Routing',      value:70,
+    },
+    {
+   text: 'You are not alone',      value:26,
+    },
+    {
+   text: 'Google API',      value:14,
+    },
+    {
+   text: 'Anonymous Texting',      value:29,
+    },
+    {
+   text: 'Google Maps',      value:41,
+    },
+    {
+   text: 'Real Time',      value:49,
+    },
+    {
+   text: 'RealTime Database',      value:20,
+    },
+    {
+   text: 'FireStore',      value:59,
+    },
+    {
+   text: 'Happy',      value:49,
+    },
+    {
+   text: 'Positive',      value:45,
+    },
+    {
+   text: 'Anonymous Texting',      value:11,
+    },
+    {
+   text: 'Authentication',      value:22,
+    },
+    {
+   text: 'Calm',      value:12,
+    },
+    {
+   text: 'Anonymous Texting',      value:38,
+    },
+    {
+   text: 'Relax',      value:54,
+    },
+    {
+   text: 'React JS',      value:14,
+    },
+    {
+   text: 'Google API',      value:41,
+    },
+    {
+   text: 'React JS',      value:24,
+    },
+    {
+   text: 'Anonymous Texting',      value:16,
+    },
+    {
+   text: 'relax',      value:29,
+    },
+    {
+   text: 'happy',      value:20,
+    },
+    {
+   text: 'connect',      value:10,
+    },
+    {
+   text: 'connect',      value:13,
+    },
+    {
+   text: 'connect',      value:35,
+    },
+    {
+   text: 'Google API',      value:59,
+    },
+    {
+   text: 'Google API',      value:32,
+    },
+    {
+   text: 'Google API',      value:17,
+    },
+    {
+   text: 'Google API',      value:77,
+    },
+    {
+   text: 'Mindful',      value:19,
+    },
+    {
+   text: 'Mindful',      value:26,
+    },
+    {
+   text: 'Mindful',      value:17,
+    },
+    {
+   text: 'Mindful',      value:22,
+    },
+    {
+   text: 'Mindful',      value:35,
+    },
+    {
+   text: 'Mindful',      value:38,
+    },
+    {
+   text: 'Mindful',      value:11,
+    },
+    {
+   text: 'Think',      value:13,
+    },
+    {
+   text: 'Think',      value:10,
+    },
+    {
+   text: 'Think',      value:49,
+    },
+    {
+   text: 'Think',      value:19,
+    },
+    {
+   text: 'Together',      value:20,
+    },
+    {
+   text: 'Together',      value:64,
+    },
+    {
+   text: 'Together',      value:54,
+    },
+    {
+   text: 'Together',      value:29,
+    },
+    {
+   text: 'Together',      value:16,
+    },
+    {
+   text: 'Together',      value:59,
+    },
+    {
+   text: 'Together',      value:49,
+    },
+    {
+   text: 'Together',      value:24,
+    },
+    {
+   text: 'Change',      value:19,
+    },
+    {
+   text: 'Change',      value:20,
+    },
+    {
+   text: 'Change',      value:29,
+    },
+    {
+   text: 'Change',      value:17,
+    },
+    {
+   text: 'Change',      value:26,
+    },
+    {
+   text: 'Change',      value:16,
+    },
+    {
+   text: 'Change',      value:64,
+    },
+    {
+   text: 'Change',      value:10,
+    },
+    {
+   text: 'Change',      value:13,
+    },
+    {
+   text: 'Restore',      value:22,
+    },
+    {
+   text: 'Restore',      value:41,
+    },
+    {
+   text: 'Restore',      value:11,
+    },
+    {
+   text: 'Restore',      value:12,
+    },
+    {
+   text: 'Restore',      value:32,
+    },
+    {
+   text: 'Restore',      value:45,
+    },
+    {
+   text: 'Restore',      value:14,
+    },
+    {
+   text: 'Restore',      value:38,
+    },
+    {
+   text: 'Restore',      value:35,
+    },
+    {
+   text: 'Peace',      value:38,
+    },
+    {
+   text: 'Peace',      value:64,
+    },
+    {
+   text: 'Peace',      value:17,
+    },
+    {
+   text: 'Peace',      value:26,
+    },
+    {
+   text: 'Peace',      value:11,
+    },
+    {
+   text: 'Peace',      value:20,
+    },
+    {
+   text: 'Peace',      value:13,
+    },
+    {
+   text: 'Talk',      value:10,
+    },
+    {
+   text: 'Talk',      value:41,
+    },
+    {
+   text: 'Talk',      value:14,
+    },
+    {
+   text: 'Talk',      value:22,
+    },
+    {
+   text: 'Talk',      value:49,
+    },
+    {
+   text: 'Talk',      value:19,
+    },
+    {
+   text: 'Feelings',      value:12,
+    },
+    {
+   text: 'Feelings',      value:16,
+    },
+    {
+   text: 'Feelings',      value:29,
+    },
+    {
+   text: 'Feelings',      value:59,
+    },
+    {
+   text: 'Feelings',      value:70,
+    },
+    {
+   text: 'Feelings',      value:35,
+    },
+    {
+   text: 'APIs',      value:54,
+    },
+    {
+   text: 'APIs',      value:20,
+    },
+    {
+   text: 'APIs',      value:64,
+    },
+    {
+   text: 'APIs',      value:10,
+    },
+    {
+   text: 'APIs',      value:12,
+    },
+    {
+   text: 'Routers',      value:22,
+    },
+    {
+   text: 'Routers',      value:11,
+    },
+    {
+   text: 'Routers',      value:13,
+    },
+    {
+   text: 'Routers',      value:32,
+    },
+    {
+   text: 'Routers',      value:26,
+    },
+    {
+   text: 'Routers',      value:19,
+    },
+    {
+   text: 'Routers',      value:17,
+    },
+    {
+   text: 'Anxiety',      value:14,
+    },
+    {
+   text: 'Anxiety',      value:38,
+    },
+    {
+   text: 'Anxiety',      value:24,
+    },
+    {
+   text: 'Anxiety',      value:59,
+    },
+    {
+   text: 'Anxiety',      value:24,
+    },
+    {
+   text: 'Anxiety',      value:29,
+    },
+    {
+   text: 'Explore',      value:13,
+    },
+    {
+   text: 'Explore',      value:17,
+    },
+    {
+   text: 'Explore',      value:14,
+    },
+    {
+   text: 'Explore',      value:12,
+    },
+    {
+   text: 'Explore',      value:11,
+    },
+    {
+   text: 'Explore',      value:32,
+    },
+    {
+   text: 'Explore',      value:10,
+    },
+  ];
+  
